fa331402d33585620d8d30ed953aa9c7a42fec86
Pragyan <pragyanpatidar14@gmail.com>
2025-01-14 22:21:21 +0530

    added transcription functionality

---
+++ src/app/[filename]/page.js
@@ -0,0 +1,17 @@
+'use client';
+import {useEffect} from "react";
+import axios from "axios";
+
+export default function FilePage({params}){
+    const filename = params.filename;
+
+    useEffect(()=>{
+        axios.get('/api/transcribe?filename='+filename);
+    },[filename]);
+
+    return(
+        <div>
+            {filename}
+        </div>
+    )
+}
--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -2,10 +2,13 @@
 import UploadIcon from "@/components/UploadIcons";
 import axios from "axios";
 import {useState} from "react";
+import {useRouter} from "next/navigation";
 
 export default function UploadForm() {
 
     const [isUploading,setIsUploading] = useState(false); // loading screen
+    const router = useRouter();
+
 
     async function upload(e){
         e.preventDefault();
@@ -16,12 +19,14 @@ export default function UploadForm() {
             setIsUploading(true);  // loading screen set to true
             const res = await axios.postForm('/api/upload',{file,});  // upload
             setIsUploading(false);  // loading screen set to false
-            console.log(res.data);
+            const newName = res.data.newName;
+            router.push('/'+newName);
         }
     }
 
     return (
         <>
+            {/*loading screen*/}
             {isUploading && (
                 <div className="bg-black/90 text-white fixed inset-0 flex items-center">
                     <div className="w-full text-center">
@@ -30,6 +35,7 @@ export default function UploadForm() {
                     </div>
                 </div>
             )}
+
             <label className="bg-green-400 py-2 px-6 rounded-full inline-flex gap-2 cursor-pointer">
                 <UploadIcon />
                 <span>Choose file</span>
