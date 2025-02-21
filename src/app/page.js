ee56d40fa48ed65f9044b610b3b57dfec37d3130
Pragyan <pragyanpatidar14@gmail.com>
2025-02-21 19:58:07 +0530

    First commit in v2 branch

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <>
 
-      <HeaderSection 
-        h1Text="Add epic captions to your videos"
+      <HeaderSection
+        h1Text="Add epic captions to your videos changes by v2"
         h2Text="Just upload your videos and we will do the rest"
       />
 
